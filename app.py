6b8f85b077fb8c4402a4067b01eecfab9b18c397
krithikraim <160115534+krithikraim@users.noreply.github.com>
2024-10-30 13:56:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, render_template
+import numpy as np
+import cv2
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
+DIR = r"C:\Users\krithik rai m\OneDrive\Desktop\cgg - Copy"
+PROTOTXT = os.path.join(DIR, r"model/colorization_deploy_v2.prototxt")
+POINTS = os.path.join(DIR, r"model/pts_in_hull.npy")
+MODEL = os.path.join(DIR, r"model/colorization_release_v2.caffemodel")
+
+# Load the model
+net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+pts = np.load(POINTS)
+class8 = net.getLayerId("class8_ab")
+conv8 = net.getLayerId("conv8_313_rh")
+pts = pts.transpose().reshape(2, 313, 1, 1)
+net.getLayer(class8).blobs = [pts.astype("float32")]
+net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+
+def colorize_image(image_path):
+    image = cv2.imread(image_path)
+    scaled = image.astype("float32") / 255.0
+    lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+    resized = cv2.resize(lab, (224, 224))
+    L = cv2.split(resized)[0]
+    L -= 50
+    net.setInput(cv2.dnn.blobFromImage(L))
+    ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+    ab = cv2.resize(ab, (image.shape[1], image.shape[0]))
+    L = cv2.split(lab)[0]
+    colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
+    colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+    colorized = np.clip(colorized, 0, 1)
+    colorized = (255 * colorized).astype("uint8")
+    output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'colorized.png')
+    cv2.imwrite(output_path, colorized)
+    return output_path
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            colorized_image_path = colorize_image(filename)
+            return render_template('index.html', original=filename, colorized=colorized_image_path)
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
